f4d369404ad196a31afd788253db78400b78050b
wanjiag <wanjiag@uoregon.edu>
2022-01-26 11:38:27 -0800

    adding edge cases

--- R/mkgroup.R
+++ R/mkgroup.R
@@ -12,7 +12,21 @@
 #'
 samplesubset <- function(nest_df, index){
 
+  if(index > nrow(nest_df)){
+    stop(paste0('Index must be a numeric integer and range between 1 and ',
+                nrow(nest_df),
+                '.'))}
+
+  if(index%%1!=0){
+    stop('Index must be an integer.')}
+
+  if(!"data" %in% colnames(nest_df)){
+    stop('The dataframe does not contain a column named "data" with data frames.
+         Please double check your data frame.
+         Did you use nest() for your data frame first?')}
+
   if(missing(index)){
+    print('random')
     n <- sample(1:nrow(nest_df), 1)
   }
   else{
@@ -22,6 +36,11 @@ samplesubset <- function(nest_df, index){
   list(subset(nest_df[n,], select = -data), nest_df$data[n][[1]])
 
 }
+
+# Warnings:
+
+#Non-standard file/directory found at top level:
+#  ‘LICENSE.md’
 
 #samplesubset: no visible binding for global variable ‘data’
 #Undefined global functions or variables:
